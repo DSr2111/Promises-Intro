7bb746d83e1e25c4ec74a69eaae2cc13b299a4bc
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-05-04 15:23:22 -0400

    getting 4 facts from one number

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const numberFact = (number) => {
 
   axios
     .get(url)
-    .then((data) => console.log(`Fact about ${number}: ${data.data}`))
+    .then((data) => {
+      console.log(`First fact about ${number}: ${data.data}`);
+      return axios.get(url);
+    })
+    .then((data) => {
+      console.log(`Second fact about ${number}: ${data.data}`);
+      return axios.get(url);
+    })
+    .then((data) => {
+      console.log(`Third fact about ${number}: ${data.data}`);
+      return axios.get(url);
+    })
+    .then((data) => {
+      console.log(`Fourth fact about ${number}: ${data.data}`);
+      return axios.get(url);
+    })
     .catch((err) => console.log(err));
 };
 
