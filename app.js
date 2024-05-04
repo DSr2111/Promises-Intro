ae8b63791f572e01ec121daf7181f31ac696eac5
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-05-04 11:06:21 -0400

    working console code

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ let url = "http://numbersapi.com/1";
 
 let numberFact = axios.get(url);
 
-numberFact.then((data) => console.log(data)).catch((err) => console.log(err));
+numberFact
+  .then((data) => console.log(`Fact about the number: ${data.data}`))
+  .catch((err) => console.log(err));
